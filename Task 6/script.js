706ec15fa5765dc480d13c601a0f3b391cf906e7
Edijs Kolesnikovičs <terminedijs@yahoo.com>
2022-04-30 07:46:33 +0300

    Task 6 sprendimas
    Dvi funkcijos, kurios priima mąsyvą, jį apdoroja ir grąžina rezultatą. Pirmoji amžiaus vidurkį, antroji - vardų mąsyvą

--- Task 6/script.js
+++ Task 6/script.js
@@ -1,7 +1,7 @@
 /* ------------------------------ TASK 6 -----------------------------------
 Turimas "users" masyvas. 
 
-Parašykite funckijas, kurios atlikas nurodytas užduotis
+Parašykite funckijas, kurios atliks nurodytas užduotis
 1. funkcija "getUserAverageAge" - kaip argumentą priims masyvą ir duoto masyvo 
 atveju grąžins visų "users" amžiaus visurkį kaip skaičių.
 2. funkcija "getUsersNames" -  kaip argumentą priims masyvą ir duoto masyvo 
@@ -19,3 +19,33 @@ const users = [
   { id: '8', name: 'Simon Peterson', age: 30 },
   { id: '9', name: 'Daniel Cane', age: 51 },
 ];
+
+function getUserAverageAge(AUsers) {
+  if (AUsers == null) {
+    return 0;
+  } else {
+    let _UserCount = 0;
+    let _AgeSum = 0;
+    AUsers.forEach((AUser, AIndex) => {
+      _UserCount = _UserCount + 1;
+      _AgeSum = _AgeSum + AUser.age;
+    });
+    return _AgeSum / _UserCount;
+  }
+};
+
+console.log(getUserAverageAge(users));
+
+function getUsersNames(AUsers) {
+  if (AUsers == null) {
+    return [];
+  } else {
+    let _UserNames = [];
+    AUsers.forEach((AUser, AIndex) => {
+      _UserNames.push(AUser.name);
+    });
+    return _UserNames;
+  }
+};
+
+console.log(getUsersNames(users));
