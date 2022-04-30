6c56929a4191b71a33888eaa5bbce24966b3e811
Edijs Kolesnikovičs <terminedijs@yahoo.com>
2022-04-30 08:18:04 +0300

    Task 8 sprendimas
    ES5 construktoriaus panauojimas paprastam skaičiuotuvui sukurti

--- Task 8/script.js
+++ Task 8/script.js
@@ -5,3 +5,27 @@ subtraction() - priima du skaičius ir grąžina jų skirtumą.
 multiplication() - priima du skaičius ir grąžina jų daugybos rezultatą;
 division() - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator(){
+    this.sum = function(ANumber1, ANumber2){
+        return ANumber1 + ANumber2;
+    }
+
+    this.subtraction = function(ANumber1, ANumber2){
+        return ANumber1 - ANumber2;
+    }
+
+    this.multiplication = function(ANumber1, ANumber2){
+        return ANumber1 * ANumber2;
+    }
+
+    this.division = function(ANumber1, ANumber2){
+        return ANumber1 / ANumber2;
+    }
+}
+
+let _Calc = new Calculator();
+console.log(`Penki + dešimt = ${_Calc.sum(5, 10)}`);
+console.log(`Dešimt - keturi = ${_Calc.subtraction(10, 4)}`);
+console.log(`Dešimt * keturi = ${_Calc.multiplication(10, 4)}`);
+console.log(`Dešimt / du = ${_Calc.division(10, 2)}`);
