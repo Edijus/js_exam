76452446980030c6cdf2301b25668c070d48897c
Edijs Kolesnikovičs <terminedijs@yahoo.com>
2022-04-30 08:57:37 +0300

    Task 10 sprendimas
    Export/import moduliai

--- Task 10/script.js
+++ Task 10/script.js
@@ -1,13 +1,18 @@
 /* ------------------------------ TASK 9 ---------------------------------------------------
 Sutvarkykite užduoties "Task 10" esančius failus taip, kad veiktų žemiau pateiktos funcijos
 -------------------------------------------------------------------------------------------- */
+import * as numbers from './modules/numbers/numbers.js';
+import { composition } from './modules/math/composition.js';
+import { division } from './modules/math/division.js';
+import { substraction } from './modules/math/subtraction.js';
+import { multiplication } from './modules/math/multiplication.js';
 
-let a = composition(one, four);
-let b = division(four, two);
-let c = substraction(three, two);
-let d = multiplication(five, two);
+let a = composition(numbers.one, numbers.four);
+let b = division(numbers.four, numbers.two);
+let c = substraction(numbers.three, numbers.two);
+let d = multiplication(numbers.five, numbers.two);
 
 console.log(a);
 console.log(b);
 console.log(c);
-console.log(c);
+console.log(d);
